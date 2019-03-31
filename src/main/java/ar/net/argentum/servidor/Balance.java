a64ce41789fef95ebd616a1e745a38ef2632fc58
Jorge Matricali <jorgematricali@gmail.com>
2019-03-31 11:40:36 -0300

    Balance: Calcular aumento de vida, de mana, de energia y de golpe

--- src/main/java/ar/net/argentum/servidor/Balance.java
+++ src/main/java/ar/net/argentum/servidor/Balance.java
@@ -31,11 +31,16 @@ public abstract class Balance {
      */
     public static final int NIVEL_MAX = 50;
 
+    public static final int[] DISTRIBUCION_VIDA_E = {10, 20, 40, 20, 10};
+    public static final int[] DISTRIBUCION_VIDA_S = {10, 40, 40, 10};
+
     public static final int COMBATE_ENERGIA_NECESARIA = 10;
+
     public static final int FRIO_INTERVALO = 5;
     public static final int FRIO_PORCENTAJE_STAMINA = 5;
     public static final int ENERGIA_INTERVALO = 5;
     public static final int ENERGIA_PORCENTAJE_RECUPERO = 5;
+    public static final int NEWBIE_NIVEL_MAXIMO = 12;
 
     /**
      * Calcular la nueva experiencia para pasar de nivel.
@@ -69,5 +74,105 @@ public abstract class Balance {
      */
     public static int calcularExperienciaGanadaAlSubirHabilidad(int nivelHabilidad) {
         return 50 * nivelHabilidad;
+    }
+
+    public static int calcularAumentoVida(String clase, int constitucion) {
+        Clase c = Servidor.getServidor().getClase(clase);
+
+        // Aumentamos la vida
+        double promedio = c.getModificadores().getVida() - (21 - constitucion) * 0.5;
+        int aux = Logica.enteroAleatorio(1, 100);
+        int[] distVida = new int[5];
+
+        if (promedio - Math.floor(promedio) == 0.5) {
+            // Es promedio semientero
+            distVida[0] = DISTRIBUCION_VIDA_S[0];
+            distVida[1] = distVida[0] + DISTRIBUCION_VIDA_S[1];
+            distVida[2] = distVida[1] + DISTRIBUCION_VIDA_S[2];
+            distVida[3] = distVida[2] + DISTRIBUCION_VIDA_S[3];
+
+            if (aux <= distVida[0]) {
+                return (int) (promedio + 1.5);
+            }
+            if (aux <= distVida[1]) {
+                return (int) (promedio + 0.5);
+            }
+            if (aux <= distVida[2]) {
+                return (int) (promedio - 0.5);
+            }
+            return (int) (promedio - 1.5);
+        }
+
+        // Promedio entero
+        distVida[0] = DISTRIBUCION_VIDA_E[0];
+        distVida[1] = distVida[0] + DISTRIBUCION_VIDA_E[1];
+        distVida[2] = distVida[1] + DISTRIBUCION_VIDA_E[2];
+        distVida[3] = distVida[2] + DISTRIBUCION_VIDA_E[3];
+        distVida[4] = distVida[3] + DISTRIBUCION_VIDA_E[4];
+
+        if (aux <= distVida[0]) {
+            return (int) (promedio + 2);
+        }
+        if (aux <= distVida[1]) {
+            return (int) (promedio + 1);
+        }
+        if (aux <= distVida[2]) {
+            return (int) (promedio);
+        }
+        if (aux <= distVida[3]) {
+            return (int) (promedio - 1);
+        }
+        return (int) (promedio - 2);
+    }
+
+    public static int calcularAumentoEnergia(String clase) {
+        int energia = 15;
+        switch (clase) {
+            case "LADRON":
+            case "BANDIDO":
+                return energia + 3;
+            case "MAGO":
+                return energia - 1;
+            case "TRABAJADOR":
+                return energia + 25;
+            default:
+                return energia;
+        }
+    }
+
+    public static int calcularAumentoMana(String clase, int inteligencia) {
+        switch (clase) {
+            case "PALADIN":
+            case "ASESINO":
+                return inteligencia;
+            case "MAGO":
+                return (int) (inteligencia * 2.8);
+            case "CLERIGO":
+            case "DRUIDA":
+            case "BARDO":
+                return inteligencia * 2;
+            case "BANDIDO":
+                return inteligencia / 2 * 3;
+            default:
+                return 0;
+        }
+    }
+    
+    public static int calcularAumentoGolpe(String clase, int nivel) {
+        switch (clase) {
+            case "GUERRERO":
+            case "CAZADOR":
+                return (nivel > 35) ? 2 : 3;
+            case "PIRATA":
+                return 3;
+            case "PALADIN":
+            case "ASESINO":
+            case "BANDIDO":
+                return (nivel > 35) ? 1 : 3;
+            case "MAGO":
+                return 1;
+            default:
+                return 2;
+        }
     }
 }
